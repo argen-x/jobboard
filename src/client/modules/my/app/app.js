b1204503f1afe2862e61e42c96e532eac03fe96f
Argen Turatbek <45792393+argenxyz@users.noreply.github.com>
2022-08-12 06:42:20 +0900

    first

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,2 +1,30 @@
+//import { response } from "express";
 import LightningElementWithBootstrap from "../../lib/lightningElementWithBootstrap";
-export default class App extends LightningElementWithBootstrap {}
+//const BASE_URL = 'https://jobs.gitub.com/positions.json';
+export default class App extends LightningElementWithBootstrap { 
+    
+
+    // description = ''
+    // location = ''
+    // full_time = false
+
+    // connectedCallback() {
+    //     this.fetchJobs();
+    // }
+
+    // fetchJobs() {
+    //     const url = `${BASE_URL}?page=1&description=${this.description}&location=${this.location}&full_time=${this.full_time}`
+    //     //this is an asynch call (fetch api) it will return a promise
+    //     fetch(url).then(response => {
+    //         response.json()
+    //         console.log(response)
+    //         }
+    //        )
+    //         .then(result => {
+    //         console.log(result)
+    //         })
+    //         .catch(error => {
+    //         console.error(error)
+    //     })
+    // }
+}
