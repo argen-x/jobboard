7700250d5bae73dae4872b976190e67b9a76c54b
Argen Turatbek <45792393+argenxyz@users.noreply.github.com>
2022-08-13 18:45:43 +0900

    addded new cs

---
+++ src/client/modules/my/jobcard/jobcard.js
@@ -0,0 +1,16 @@
+import LightningElementWithBootstrap from "../../lib/lightningElementWithBootstrap";
+import { api } from 'lwc'
+import {dateFormatter} from '../../utils/utils'
+export default class Jobcard extends LightningElementWithBootstrap {
+    @api job
+
+    get formattedDate() {
+        return dateFormatter(this.job.created_at)
+    }
+
+    viewDetailHandler() {
+        this.dispatchEvent(new CustomEvent('selected', {
+            detail:this.job
+        }))
+    }
+}
